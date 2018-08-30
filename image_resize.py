9bf02e8ef25e8d8fee2603d67a1c39382d0e58b5
Nicolay Poymanov <n.poymanov@gmail.com>
2018-08-30 21:09:59 +0300

    fix comments

--- image_resize.py
+++ image_resize.py
@@ -5,19 +5,26 @@ from PIL import Image
 
 
 def get_args_descriptions():
-    return {'file': 'Path to image file',
-            'width': 'The width of the processed image',
-            'height': 'The height of the processed image',
-            'scale': 'Image scale ratio',
-            'output': 'Path to save resized image'}
+    descr = {}
+    descr['file'] = 'Path to image file'
+    descr['width'] = 'The width of the processed image'
+    descr['height'] = 'The height of the processed image'
+    descr['scale'] = 'Image scale ratio'
+    descr['output'] = 'Path to save resized image'
+
+    return descr
 
 
 def validate_arguments(args):
-    if args.width == args.height == args.scale is None:
+    width = args.width
+    height = args.height
+    scale = args.scale
+
+    if width == height == scale is None:
         return 'You must specify at least one argument.'
-    elif (args.width or args.height) and args.scale:
+    elif (width or height) and scale:
         return 'You must specify only width/height or scale. Not both.'
-    elif 0 in [args.width, args.height, args.scale]:
+    elif not all(item is None or item > 0 for item in [width, height, scale]):
         return 'Values of arguments must be greater than 0'
 
 
@@ -48,81 +55,100 @@ def get_image_object(filepath):
         return None
 
 
-def check_proportions(origin_ratio, new_ratio):
-    ratio_delta = abs(origin_ratio - new_ratio)
+def check_resized_proportions(new_sizes, params):
+    new_width, new_height = new_sizes
+    new_ratio = get_proporsion_ratio(new_width, new_height)
+
+    ratio_delta = abs(params['origin_ratio'] - new_ratio)
     allowed_ratio_delta = 0.1
 
     if ratio_delta > allowed_ratio_delta:
-        print(('Attention! The proportions of the new image do not match'
-               'the proportions of the original'))
+        print('Attention! The proportions of the new image do not match'
+              'the proportions of the original')
 
 
 def get_proporsion_ratio(width, height):
     return width / height
 
 
-def get_resize_info(image_object, width, height, scale):
-    origin_proporsion_ratio = get_proporsion_ratio(image_object.width,
-                                                   image_object.height)
+def get_resize_info(params):
+    width = params['width']
+    height = params['height']
+    scale = params['scale']
 
     if scale:
-        new_width = int(image_object.width * scale)
-        new_height = int(image_object.height * scale)
+        new_width = int(params['image_width'] * scale)
+        new_height = int(params['image_height'] * scale)
     elif width and height:
         new_width = width
         new_height = height
-        resized_proporsion_ratio = get_proporsion_ratio(new_width, new_height)
-        check_proportions(origin_proporsion_ratio, resized_proporsion_ratio)
     elif width:
         new_width = width
-        new_height = int(width / origin_proporsion_ratio)
+        new_height = int(width / params['origin_ratio'])
     elif height:
-        new_width = int(height * origin_proporsion_ratio)
+        new_width = int(height * params['origin_ratio'])
         new_height = height
 
     return new_width, new_height
 
 
-def resize_image(image_object, width, height):
-    return image_object.resize((width, height))
+def resize_image(params, new_sizes):
+    return params['image'].resize(new_sizes)
 
 
-def save_image(image_object, origin_filepath, output_filepath):
-    origin_filename, origin_file_extension = os.path.splitext(origin_filepath)
-    image_width = image_object.width
-    image_height = image_object.height
-    new_filename = '{}__{}x{}{}'.format(origin_filename, image_width,
-                                        image_height, origin_file_extension)
+def save_image(image, params):
+    filename, extension = os.path.splitext(params['file'])
+    width = image.width
+    height = image.height
+    new_filename = '{}__{}x{}{}'.format(filename, width, height, extension)
 
-    if output_filepath:
-        new_filepath = os.path.join(output_filepath, new_filename)
+    if params['output']:
+        new_filepath = os.path.join(params['output'], new_filename)
     else:
         new_filepath = new_filename
 
     try:
-        image_object.save(new_filepath)
+        image.save(new_filepath)
         return new_filepath
     except IOError:
         return None
 
 
+def get_resize_params(image, args):
+    params = {}
+    params['image'] = image
+    params['file'] = args.file
+    params['width'] = args.width
+    params['height'] = args.height
+    params['scale'] = args.scale
+    params['output'] = args.output
+    params['image_width'] = image.width
+    params['image_height'] = image.height
+    params['origin_ratio'] = get_proporsion_ratio(image.width, image.height)
+
+    return params
+
+
 if __name__ == '__main__':
     args = parse_args()
 
-    image_object = get_image_object(args.file)
+    image = get_image_object(args.file)
 
-    if not image_object:
-        sys.exit(('Failed to open image: '
-                  'file not found or have got wrong file format'))
+    if not image:
+        sys.exit('Failed to open image: '
+                 'file not found or have got wrong file format')
 
-    new_width, new_height = get_resize_info(image_object, args.width,
-                                            args.height, args.scale)
+    resize_params = get_resize_params(image, args)
 
-    resized_image = resize_image(image_object, new_width, new_height)
+    new_sizes = get_resize_info(resize_params)
 
-    save_status = save_image(resized_image, args.file, args.output)
+    check_resized_proportions(new_sizes, resize_params)
+
+    resized_image = resize_image(resize_params, new_sizes)
+
+    save_status = save_image(resized_image, resize_params)
 
     if not save_status:
         sys.exit('Failed to save resized image')
     else:
-        sys.exit('Resized file saved to: ' + save_status)
+        sys.exit('Resized file saved to: {}'.format(save_status))
