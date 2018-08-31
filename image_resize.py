5576e07813a9f0ecccba4155c6df6c2834ba8b25
Nicolay Poymanov <n.poymanov@gmail.com>
2018-08-31 20:10:18 +0300

    fix comments

--- image_resize.py
+++ image_resize.py
@@ -5,14 +5,14 @@ from PIL import Image
 
 
 def get_args_descriptions():
-    descr = {}
-    descr['file'] = 'Path to image file'
-    descr['width'] = 'The width of the processed image'
-    descr['height'] = 'The height of the processed image'
-    descr['scale'] = 'Image scale ratio'
-    descr['output'] = 'Path to save resized image'
+    descriptions = {}
+    descriptions['file'] = 'Path to image file'
+    descriptions['width'] = 'The width of the processed image'
+    descriptions['height'] = 'The height of the processed image'
+    descriptions['scale'] = 'Image scale ratio'
+    descriptions['output'] = 'Path to save resized image'
 
-    return descr
+    return descriptions
 
 
 def validate_arguments(args):
@@ -30,13 +30,13 @@ def validate_arguments(args):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    descr = get_args_descriptions()
+    descriptions = get_args_descriptions()
 
-    parser.add_argument('file', help=descr['file'])
-    parser.add_argument('--width', help=descr['width'], type=int)
-    parser.add_argument('--height', help=descr['height'], type=int)
-    parser.add_argument('--scale', help=descr['scale'], type=float)
-    parser.add_argument('--output', help=descr['output'], type=str)
+    parser.add_argument('file', help=descriptions['file'])
+    parser.add_argument('--width', help=descriptions['width'], type=int)
+    parser.add_argument('--height', help=descriptions['height'], type=int)
+    parser.add_argument('--scale', help=descriptions['scale'], type=float)
+    parser.add_argument('--output', help=descriptions['output'], type=str)
 
     parsed_args = parser.parse_args()
     validation_status = validate_arguments(parsed_args)
@@ -55,11 +55,14 @@ def get_image_object(filepath):
         return None
 
 
-def check_resized_proportions(new_sizes, params):
+def check_resized_proportions(origin_sizes, new_sizes):
+    origin_width, origin_height = origin_sizes
+    origin_ratio = get_proporsion_ratio(origin_width, origin_height)
+
     new_width, new_height = new_sizes
     new_ratio = get_proporsion_ratio(new_width, new_height)
 
-    ratio_delta = abs(params['origin_ratio'] - new_ratio)
+    ratio_delta = abs(origin_ratio - new_ratio)
     allowed_ratio_delta = 0.1
 
     if ratio_delta > allowed_ratio_delta:
@@ -71,39 +74,38 @@ def get_proporsion_ratio(width, height):
     return width / height
 
 
-def get_resize_info(params):
-    width = params['width']
-    height = params['height']
-    scale = params['scale']
+def get_new_image_size(origin_sizes, width, height, scale):
+    image_width, image_height = origin_sizes
+    origin_ratio = get_proporsion_ratio(image_width, image_height)
 
     if scale:
-        new_width = int(params['image_width'] * scale)
-        new_height = int(params['image_height'] * scale)
+        new_width = int(image_width * scale)
+        new_height = int(image_height * scale)
     elif width and height:
         new_width = width
         new_height = height
     elif width:
         new_width = width
-        new_height = int(width / params['origin_ratio'])
+        new_height = int(width / origin_ratio)
     elif height:
-        new_width = int(height * params['origin_ratio'])
+        new_width = int(height * origin_ratio)
         new_height = height
 
     return new_width, new_height
 
 
-def resize_image(params, new_sizes):
-    return params['image'].resize(new_sizes)
+def resize_image(image, new_sizes):
+    return image.resize(new_sizes)
 
 
-def save_image(image, params):
-    filename, extension = os.path.splitext(params['file'])
+def save_image(image, file, output_path):
+    filename, extension = os.path.splitext(file)
     width = image.width
     height = image.height
     new_filename = '{}__{}x{}{}'.format(filename, width, height, extension)
 
-    if params['output']:
-        new_filepath = os.path.join(params['output'], new_filename)
+    if output_path:
+        new_filepath = os.path.join(output_path, new_filename)
     else:
         new_filepath = new_filename
 
@@ -112,21 +114,6 @@ def save_image(image, params):
         return new_filepath
     except IOError:
         return None
-
-
-def get_resize_params(image, args):
-    params = {}
-    params['image'] = image
-    params['file'] = args.file
-    params['width'] = args.width
-    params['height'] = args.height
-    params['scale'] = args.scale
-    params['output'] = args.output
-    params['image_width'] = image.width
-    params['image_height'] = image.height
-    params['origin_ratio'] = get_proporsion_ratio(image.width, image.height)
-
-    return params
 
 
 if __name__ == '__main__':
@@ -138,15 +125,16 @@ if __name__ == '__main__':
         sys.exit('Failed to open image: '
                  'file not found or have got wrong file format')
 
-    resize_params = get_resize_params(image, args)
+    origin_sizes = (image.width, image.height)
 
-    new_sizes = get_resize_info(resize_params)
+    new_sizes = get_new_image_size(origin_sizes, args.width,
+                                   args.height, args.scale)
 
-    check_resized_proportions(new_sizes, resize_params)
+    check_resized_proportions(origin_sizes, new_sizes)
 
-    resized_image = resize_image(resize_params, new_sizes)
+    resized_image = resize_image(image, new_sizes)
 
-    save_status = save_image(resized_image, resize_params)
+    save_status = save_image(resized_image, args.file, args.output)
 
     if not save_status:
         sys.exit('Failed to save resized image')
